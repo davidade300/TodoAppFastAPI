b7c4e12879a0ff63fa0df2a9c0f6da13f3a6aecc
daviade300 <iaderinho@icloud.com>
2024-06-11 17:09:54 -0300

    added tests for password and phone number

--- test/test_users.py
+++ test/test_users.py
@@ -9,7 +9,7 @@ app.dependency_overrides[get_current_user] = override_get_current_user
 
 
 def test_return_user(test_user):
-    """test for user"""
+    """tests for user"""
     response = client.get("/user")
     assert response.status_code == status.HTTP_200_OK
     assert response.json()["email"] == "davidade@mail.com"
@@ -17,3 +17,26 @@ def test_return_user(test_user):
     assert response.json()["last_name"] == "Oliveira"
     assert response.json()["role"] == "admin"
     assert response.json()["phone_number"] == "(11) 1 1111 1111"
+
+
+def test_change_password_success(test_user):
+    """tests for password changes"""
+    response = client.put("/user/password", json={"password": "123456",
+                                                  "new_password": "1234567"})
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+def test_change_password_invalid_current_password(test_user):
+    """tests for password changes with a invalid user"""
+    response = client.put("/user/password", json={"password": "wrong_pwd",
+                                                  "new_password": "1234567"})
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {"detail": "Error on password change"}
+
+
+def test_change_phone_number_success(test_user):
+    """tests for phone number change"""
+    response = client.put("/user/phone_number/222222222")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
