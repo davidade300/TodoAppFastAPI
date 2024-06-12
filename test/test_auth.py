97945329ba9d4b29ca082fabd3b01750686616c7
daviade300 <iaderinho@icloud.com>
2024-06-12 17:45:24 -0300

    async tests for current user added

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,7 +1,13 @@
 """file with tests for auth"""
-from routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM
-from jose import jwt
 from datetime import timedelta
+
+import pytest
+from fastapi import HTTPException
+from jose import jwt
+
+from routers.auth import (ALGORITHM, SECRET_KEY, authenticate_user,
+                          create_access_token, get_current_user, get_db)
+
 from .utils import *
 
 app.dependency_overrides[get_db] = override_get_db
@@ -38,3 +44,34 @@ def test_create_access_token():
     assert decoded_token["sub"] == username
     assert decoded_token["id"] == user_id
     assert decoded_token["role"] == role
+
+
+@pytest.mark.asyncio  # marks the function as an async application
+async def test_get_current_user_valid_token():
+    """
+    when testing for an async function you should
+    use a async test function
+    """
+
+    encode = {"sub": "testuser", "id": 1, "role": "admin"}
+    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    # since get_current_user is asyn you should use await
+    user = await get_current_user(token=token)
+    assert user == {"username": "testuser", "id": 1, "user_role": "admin"}
+
+
+@pytest.mark.asyncio
+async def test_get_current_user_missing_payload():
+    """
+    test for --if-- an exception happens calls the exception
+    that says that the user is not validated
+    """
+    encode = {"role": "user"}
+    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    with pytest.raises(HTTPException) as excinfo:
+        await get_current_user(token=token)
+
+    assert excinfo.value.status_code == 401
+    assert excinfo.value.detail == "Could not valide user!"
